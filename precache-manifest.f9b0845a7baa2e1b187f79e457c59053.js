0f4b3fdbb9458c6615526a4c5a4c9dcca34661e8
Minserku <minserku>
2020-02-10 15:33:25 +0900

    Updates

---
+++ precache-manifest.f9b0845a7baa2e1b187f79e457c59053.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feddda999da3621c1b965b06654e7967",
+    "url": "/webapp/index.html"
+  },
+  {
+    "revision": "b6a48225ea1235d0d12b",
+    "url": "/webapp/static/css/main.0ad1a71d.chunk.css"
+  },
+  {
+    "revision": "25157d8b49a4fafd5cde",
+    "url": "/webapp/static/js/2.f35bd835.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/webapp/static/js/2.f35bd835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a48225ea1235d0d12b",
+    "url": "/webapp/static/js/main.9c3fdbae.chunk.js"
+  },
+  {
+    "revision": "784acc8abd50b15bf86e",
+    "url": "/webapp/static/js/runtime-main.344f6ea8.js"
+  },
+  {
+    "revision": "43b7af8d647a31577167ff5831a9fe3d",
+    "url": "/webapp/static/media/Aeye.43b7af8d.png"
+  },
+  {
+    "revision": "c85a28cf6587d82cff131ff9d6cdcd2b",
+    "url": "/webapp/static/media/Cowboy.c85a28cf.png"
+  }
+]);
